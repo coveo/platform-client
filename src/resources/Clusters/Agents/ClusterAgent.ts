3574bfa1af3a7a129bcc77a175c0120cc069d6e3
pochretien <moipeter10@gmail.com>
2019-10-23 08:47:24 -0400

    feat(clusters): add agents and nodes

---
+++ src/resources/Clusters/Agents/ClusterAgent.ts
@@ -0,0 +1,17 @@
+import API from '../../../APICore';
+import Resource from '../../Resource';
+import {ClusterNodeUpgradeDataModel} from '../ClusterInterfaces';
+import {ClusterNodeUpgradeModel} from '../Nodes';
+import {ClusterAgentModel} from './ClusterAgentInterfaces';
+
+export default class ClusterAgent extends Resource {
+    static getBaseUrl = (id: string) => `/rest/organizations/${API.orgPlaceholder}/clusters/${id}/agents`;
+
+    list(clusterId: string) {
+        return this.api.get<ClusterAgentModel[]>(ClusterAgent.getBaseUrl(clusterId));
+    }
+
+    upgrade(clusterId: string, id: string, data: ClusterNodeUpgradeDataModel) {
+        return this.api.put<ClusterNodeUpgradeModel[]>(`${ClusterAgent.getBaseUrl(clusterId)}/${id}/upgrades`, data);
+    }
+}
---
+++ src/resources/Clusters/Nodes/ClusterNode.ts
@@ -0,0 +1,32 @@
+import API from '../../../APICore';
+import Resource from '../../Resource';
+import {ClusterNodeUpgradeDataModel} from '../ClusterInterfaces';
+import {ClusterNodeModel, ClusterNodeUpgradeModel} from './ClusterNodeInterfaces';
+
+export default class ClusterNode extends Resource {
+    static getBaseUrl = (id: string) => `/rest/organizations/${API.orgPlaceholder}/clusters/${id}/nodes`;
+
+    list(clusterId: string) {
+        return this.api.get<ClusterNodeModel[]>(ClusterNode.getBaseUrl(clusterId));
+    }
+
+    listUpgrades(clusterId: string, id: string) {
+        return this.api.get<ClusterNodeUpgradeModel[]>(`${ClusterNode.getBaseUrl(clusterId)}/${id}/upgrades`);
+    }
+
+    start(clusterId: string, id: string) {
+        return this.api.post<{}>(`${ClusterNode.getBaseUrl(clusterId)}/${id}/start`, {});
+    }
+
+    stop(clusterId: string, id: string) {
+        return this.api.post<{}>(`${ClusterNode.getBaseUrl(clusterId)}/${id}/stop`, {});
+    }
+
+    dump(clusterId: string, id: string) {
+        return this.api.post<{}>(`${ClusterNode.getBaseUrl(clusterId)}/${id}/dumps`, {});
+    }
+
+    upgrade(clusterId: string, id: string, data: ClusterNodeUpgradeDataModel) {
+        return this.api.put<ClusterNodeUpgradeModel[]>(`${ClusterNode.getBaseUrl(clusterId)}/${id}/upgrades`, data);
+    }
+}
