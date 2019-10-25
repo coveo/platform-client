0e87e917c8158eb3534c3eacccff72040e7b8ae9
Germain Bergeron <germain.bergeron1@gmail.com>
2019-10-25 15:22:39 -0400

    fix(aws): fix missing filter on organization

--- src/resources/AWS/AWS.ts
+++ src/resources/AWS/AWS.ts
@@ -5,6 +5,8 @@ export default class AWS extends Resource {
     static baseUrl = `/rest/aws/instances/agents`;
 
     list() {
-        return this.api.get<AWSAgentInstanceModel[]>(AWS.baseUrl);
+        return this.api.get<AWSAgentInstanceModel[]>(
+            this.buildPath(AWS.baseUrl, {organizationId: this.api.organizationId})
+        );
     }
 }
