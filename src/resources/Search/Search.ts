92fb9e73401745592abe99f552c6302d57188062
pocketpapa <danny.m.gauthier@gmail.com>
2020-02-17 14:37:36 -0500

    fix(search api ressource): fix  the base path

--- src/resources/Search/Search.ts
+++ src/resources/Search/Search.ts
@@ -3,7 +3,7 @@ import Ressource from '../Resource';
 import {RestTokenParams, TokenModel} from './SearchInterfaces';
 
 export default class Search extends Ressource {
-    static baseUrl = `rest/search/v2`;
+    static baseUrl = `/rest/search/v2`;
 
     createToken(tokenParams: RestTokenParams) {
         return this.api.post<TokenModel>(`${Search.baseUrl}/token?organizationId=${API.orgPlaceholder}`, tokenParams);
