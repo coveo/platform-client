a3275e92aad058aff2ce16aa4028a96885b8c33b
Gaël Dostie <gdostie@coveo.com>
2020-02-25 14:30:01 -0500

    feat(pipeline): add pipelines main calls
    Added the following calls on the `pipeline` resource:
    - `list`
    - `get`
    - `delete`
    - `update`
    - `duplicate`
    - `create`
    
    BREAKING CHANGE: `pipeline.listBasicInfo` has been replaced by `pipeline.list`
    BREAKING CHANGE: `pipeline.getBackendVersion` renamed to `getMLVersion`

--- src/resources/Pipelines/tests/Pipelines.spec.ts
+++ src/resources/Pipelines/tests/Pipelines.spec.ts
@@ -1,5 +1,6 @@
 import API from '../../../APICore';
 import Pipelines from '../Pipelines';
+import {NewPipelineModel, UpdatePipelineModel} from '../PipelinesInterfaces';
 
 jest.mock('../../../APICore');
 
@@ -14,19 +15,72 @@ describe('Pipelines', () => {
         pipelines = new Pipelines(api);
     });
 
-    describe('listBasicInfo', () => {
-        it('should make a GET call to the specific Pipelines url', () => {
-            pipelines.listBasicInfo();
+    describe('list', () => {
+        it('should make a GET call to the Pipelines v1 url', () => {
+            pipelines.list();
             expect(api.get).toHaveBeenCalledTimes(1);
-            expect(api.get).toHaveBeenCalledWith(Pipelines.searchUrl);
+            expect(api.get).toHaveBeenCalledWith(Pipelines.searchUrlVersion1);
         });
     });
 
     describe('getBackendVersion', () => {
         it('should make a GET call to the specific Pipelines url', () => {
-            pipelines.getBackendVersion();
+            pipelines.getMLVersion();
             expect(api.get).toHaveBeenCalledTimes(1);
             expect(api.get).toHaveBeenCalledWith(`${Pipelines.searchUrlVersion2}/ml/version`);
+        });
+    });
+
+    describe('get', () => {
+        it('should make a GET call to /rest/searc/v1/admin/pipelines/:id', () => {
+            pipelines.get('🔥');
+
+            expect(api.get).toHaveBeenCalledTimes(1);
+            expect(api.get).toHaveBeenCalledWith('/rest/search/v1/admin/pipelines/🔥');
+        });
+    });
+
+    describe('delete', () => {
+        it('should make a DELETE call to /rest/searc/v1/admin/pipelines/:id', () => {
+            pipelines.delete('🔥');
+
+            expect(api.delete).toHaveBeenCalledTimes(1);
+            expect(api.delete).toHaveBeenCalledWith('/rest/search/v1/admin/pipelines/🔥');
+        });
+    });
+
+    describe('update', () => {
+        it('should make a PUT call to /rest/searc/v1/admin/pipelines/:id', () => {
+            const pipelineToUpdate: UpdatePipelineModel = {
+                id: '🔥',
+                name: 'fire',
+            };
+            pipelines.update(pipelineToUpdate);
+
+            expect(api.put).toHaveBeenCalledTimes(1);
+            expect(api.put).toHaveBeenCalledWith('/rest/search/v1/admin/pipelines/🔥', pipelineToUpdate);
+        });
+    });
+
+    describe('duplicate', () => {
+        it('should make a POST call to /rest/searc/v1/admin/pipelines/:id/duplicate', () => {
+            pipelines.duplicate('🔥');
+
+            expect(api.post).toHaveBeenCalledTimes(1);
+            expect(api.post).toHaveBeenCalledWith('/rest/search/v1/admin/pipelines/🔥/duplicate');
+        });
+    });
+
+    describe('create', () => {
+        it('should make a POST call to /rest/searc/v1/admin/pipelines', () => {
+            const newPipeline: NewPipelineModel = {
+                name: 'fire',
+                description: 'this-is-lit',
+            };
+            pipelines.create(newPipeline);
+
+            expect(api.post).toHaveBeenCalledTimes(1);
+            expect(api.post).toHaveBeenCalledWith('/rest/search/v1/admin/pipelines', newPipeline);
         });
     });
 });
