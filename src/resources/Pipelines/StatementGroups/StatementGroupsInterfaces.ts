67d459dc8f80a4f3cc67c3d56cf5315f2fb5debd
Rosalie Liu <52677246+rosalie-liu@users.noreply.github.com>
2020-05-05 12:21:10 -0400

    feat(statementgroups): update resource (#109)

--- src/resources/Pipelines/StatementGroups/StatementGroupsInterfaces.ts
+++ src/resources/Pipelines/StatementGroups/StatementGroupsInterfaces.ts
@@ -3,18 +3,37 @@ import {StatementGroupType} from '../../Enums';
 export interface StatementGroupList {
     groups: StatementGroupModel[];
     totalCount: number;
+    groupComposition: PipelineGroupsComposition;
 }
 
 export interface StatementGroupModel {
     id: string;
     name: string;
     type: StatementGroupType;
-    isActive: boolean;
+    isActive?: boolean;
     campaignStart?: string;
     campaignEnd?: string;
     description?: string;
     conditionId?: string;
     conditionDefinition?: string;
+    statementComposition: StatementGroupComposition;
+}
+
+export interface PipelineGroupsComposition {
+    activeGroupCount: number;
+    inactiveGroupCount: number;
+    campaignGroupCount: number;
+    permanentGroupCount: number;
+}
+
+export interface StatementGroupComposition {
+    resultRankingStatementCount: number;
+    otherStatementCount: number;
+}
+
+export interface ListStatementGroupsOptions {
+    page?: number;
+    perPage?: number;
 }
 
 export interface CreateStatementGroupModel {
@@ -22,7 +41,10 @@ export interface CreateStatementGroupModel {
     type: StatementGroupType;
     campaignStart?: string;
     campaignEnd?: string;
-    campaignTimezone?: string;
     description?: string;
     conditionId?: string;
+}
+
+export interface UpdateStatementGroupModel extends CreateStatementGroupModel {
+    isActive?: boolean;
 }
