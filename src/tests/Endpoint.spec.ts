a980402ce1740d8c9c3495275d392a3290bd7054
Gaël Dostie <gdostie@coveo.com>
2020-05-22 12:57:33 -0400

    feat(endpoints): add the region parameter to support multi-region

---
+++ src/tests/Endpoint.spec.ts
@@ -0,0 +1,60 @@
+import getEndpoint, {Environment, Region} from '../Endpoints';
+
+describe('Endpoint', () => {
+    it('should return the prod US endpoint by default', () => {
+        expect(getEndpoint()).toBe('https://platform.cloud.coveo.com');
+    });
+
+    describe('development environment', () => {
+        it('should return the development US endpoint when no region is specified', () => {
+            expect(getEndpoint(Environment.dev)).toBe('https://platformdev.cloud.coveo.com');
+        });
+
+        it('should not add a region suffix for the US region', () => {
+            expect(getEndpoint(Environment.dev, Region.US)).toBe('https://platformdev.cloud.coveo.com');
+        });
+
+        it('should return the endpoint corresponding to each specified environment and region', () => {
+            expect(getEndpoint(Environment.dev, Region.EU)).toBe('https://platformdev-eu.cloud.coveo.com');
+        });
+    });
+
+    describe('staging environment', () => {
+        it('should return the development US endpoint when no region is specified', () => {
+            expect(getEndpoint(Environment.staging)).toBe('https://platformqa.cloud.coveo.com');
+        });
+
+        it('should not add a region suffix for the US region', () => {
+            expect(getEndpoint(Environment.staging, Region.US)).toBe('https://platformqa.cloud.coveo.com');
+        });
+
+        it('should return the endpoint corresponding to each specified environment and region', () => {
+            expect(getEndpoint(Environment.staging, Region.EU)).toBe('https://platformqa-eu.cloud.coveo.com');
+        });
+    });
+
+    describe('prod environment', () => {
+        it('should return the development US endpoint when no region is specified', () => {
+            expect(getEndpoint(Environment.prod)).toBe('https://platform.cloud.coveo.com');
+        });
+
+        it('should not add a region suffix for the US region', () => {
+            expect(getEndpoint(Environment.prod, Region.US)).toBe('https://platform.cloud.coveo.com');
+        });
+
+        it('should return the endpoint corresponding to each specified environment and region', () => {
+            expect(getEndpoint(Environment.prod, Region.EU)).toBe('https://platform-eu.cloud.coveo.com');
+        });
+    });
+
+    describe('hipaa environment', () => {
+        it('should return the development US endpoint when no region is specified', () => {
+            expect(getEndpoint(Environment.hipaa)).toBe('https://platformhipaa.cloud.coveo.com');
+        });
+
+        it('should not add a region suffix for any region', () => {
+            expect(getEndpoint(Environment.hipaa, Region.US)).toBe('https://platformhipaa.cloud.coveo.com');
+            expect(getEndpoint(Environment.hipaa, Region.EU)).toBe('https://platformhipaa.cloud.coveo.com');
+        });
+    });
+});
