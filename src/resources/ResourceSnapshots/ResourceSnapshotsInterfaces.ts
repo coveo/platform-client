3fc63d9bdbcd1166cafd6d08902e7afe41484a4e
Jean-Michel Provencher <jmprovencher@coveo.com>
2020-06-01 16:48:26 -0400

    feat(resourcesnapshots): adding dryrun call

--- src/resources/ResourceSnapshots/ResourceSnapshotsInterfaces.ts
+++ src/resources/ResourceSnapshots/ResourceSnapshotsInterfaces.ts
@@ -5,6 +5,39 @@ export interface ResourceSnapshotsModel {
     id: string;
     originId?: string;
     targetId?: string;
+    reports?: ResourceSnapshotsReportModel[];
+}
+
+export enum ResourceSnapshotsReportResultCode {
+    Success = 'SUCCESS',
+    UnexpectedError = 'UNEXPECTED_ERROR',
+    UnableToPushToOrganization = 'UNABLE_TO_PUSH_TO_ORGANIZATION',
+    NoResourceRetrieved = 'NO_RESOURCE_RETRIEVED',
+    UnableToRetrieveResources = 'UNABLE_TO_RETRIEVE_RESOURCES',
+}
+
+export enum ResourceSnapshotsReportStatus {
+    Pending = 'PENDING',
+    InProgress = 'IN_PROGRESS',
+    Completed = 'COMPLETED',
+    Aborted = 'ABORTED',
+}
+
+export enum ResourceSnapshotsReportType {
+    CreateSnapshot = 'CREATE_SNAPSHOT',
+    DryRun = 'DRY_RUN',
+    Apply = 'APPLY',
+}
+
+export interface ResourceSnapshotsReportModel {
+    id: string;
+    resourceOperationResults?: object;
+    resourceOperations?: object;
+    resourcesProcessed?: number;
+    resultCode: ResourceSnapshotsReportResultCode;
+    status: ResourceSnapshotsReportStatus;
+    type: ResourceSnapshotsReportType;
+    updatedDate: number;
 }
 
 export interface ResourceSnapshotUrlModel {
@@ -15,4 +48,8 @@ export interface ResourceSnapshotUrlModel {
 export interface PushSnapshotOptions {
     targetOrganizationId: string;
     developerNotes?: string;
+}
+
+export interface DryRunOptions {
+    deleteMissingResources: boolean;
 }
