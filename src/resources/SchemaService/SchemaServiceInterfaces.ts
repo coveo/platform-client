e19952e116a178c7d15113f7283c425598f5cdd4
David Guerin <dguerin@coveo.com>
2020-08-28 08:37:31 -0400

    refactor: make objectsToGet keys facultative
    Add an objectsTOGetCOnfiguration key in createShemaSourceModel
    
    SNOW-398

--- src/resources/SchemaService/SchemaServiceInterfaces.ts
+++ src/resources/SchemaService/SchemaServiceInterfaces.ts
@@ -33,39 +33,41 @@ export interface SchemaServiceQueryParams {
 }
 
 export interface ObjectsToGetField {
-    name: string;
+    name?: string;
 }
 
 export interface ObjectsToGetRelation {
-    name: string;
-    fields: string[];
-    fromField: string;
-    toField: string;
-    toObject: string;
+    name?: string;
+    fields?: string[];
+    fromField?: string;
+    toField?: string;
+    toObject?: string;
 }
 export interface ObjectsToGetCondition {
-    field: string;
-    operator: string;
-    type: string;
-    values: string[];
+    field?: string;
+    operator?: string;
+    type?: string;
+    values?: string[];
 }
 
 export interface ObjectsToGetObject {
     name: string;
-    conditions: ObjectsToGetCondition[];
-    fields: ObjectsToGetField[];
-    relations: ObjectsToGetRelation[];
-    attachment: string;
+    conditions?: ObjectsToGetCondition[];
+    fields?: ObjectsToGetField[];
+    relations?: ObjectsToGetRelation[];
+    attachment?: string;
 }
 
 export interface ObjectsToGet {
-    objects: ObjectsToGetObject[];
+    objects?: ObjectsToGetObject[];
 }
 
 export interface SchemaServiceSource extends SourceModel {
-    objectsToGet: ObjectsToGet;
+    objectsToGet?: ObjectsToGet;
 }
 
-export interface CreateSchemaSourceModel extends CreateSourceModel {}
+export interface CreateSchemaSourceModel extends CreateSourceModel {
+    objectsToGetConfiguration?: string;
+}
 
 export interface CreateSchemaSourceOptions extends CreateSourceOptions {}
