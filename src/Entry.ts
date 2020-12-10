418e003b476c58206e1ded5ee141ea6d5268bca1
Gaël Dostie <gdostie@coveo.com>
2020-12-10 18:40:04 -0500

    fix(build): add default export in the entry file
    The PlatformClient class was not exported as default in the Entry, making it harder to use.

--- src/Entry.ts
+++ src/Entry.ts
@@ -1,3 +1,5 @@
+import PlatformClient from './PlatformClient';
+
 export * from './PlatformClient';
 export * from './ConfigurationInterfaces';
 export * from './APICore';
@@ -5,3 +7,5 @@ export * from './handlers';
 export * from './resources';
 export * from './utils';
 export * from './Endpoints';
+
+export default PlatformClient;
