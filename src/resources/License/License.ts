a78255dae53d7ca96de5bab6f3a93652089b9176
Jack <jmcgregor@coveo.com>
2020-12-22 09:36:11 -0500

    feat(license): added a call to update the whole license
    in swagger theres an endpoint to update the whole license, but theres no call to this endpoint in
    platform-client

--- src/resources/License/License.ts
+++ src/resources/License/License.ts
@@ -4,8 +4,8 @@ import {LicenseModel} from './LicenseInterfaces';
 import API from '../../APICore';
 
 export default class License extends Resource {
-    static getBaseUrl = (sectionName: LicenseSection) =>
-        `/rest/organizations/${API.orgPlaceholder}/license/${sectionName}`;
+    static getBaseUrl = (sectionName?: LicenseSection) =>
+        `/rest/organizations/${API.orgPlaceholder}/license${sectionName ? `/${sectionName}` : ''}`;
 
     get(sectionName: LicenseSection) {
         return this.api.get<LicenseModel>(License.getBaseUrl(sectionName));
@@ -13,5 +13,9 @@ export default class License extends Resource {
 
     update(sectionName: LicenseSection, licenseSection: Record<string, number>) {
         return this.api.put<LicenseModel>(License.getBaseUrl(sectionName), licenseSection);
+    }
+
+    updateLicense(license: LicenseModel) {
+        return this.api.put<LicenseModel>(License.getBaseUrl(), license);
     }
 }
