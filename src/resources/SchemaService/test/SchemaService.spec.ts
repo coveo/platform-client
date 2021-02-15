c9d5beeaa78a29835e3025eefdb3b90fc8468f3d
pathiery <pathiery@coveo.com>
2021-02-15 05:39:25 -0500

    fix(schema update)!: make those tests pass SNOW-544

--- src/resources/SchemaService/test/SchemaService.spec.ts
+++ src/resources/SchemaService/test/SchemaService.spec.ts
@@ -34,10 +34,10 @@ describe('SchemaService', () => {
 
     describe('getEntities', () => {
         it('should make a GET call to the specific SchemaService url with the correct params', () => {
-            schemaService.getEntities(sourceType, params);
+            schemaService.getEntities(sourceType, {...params, offset: 100, limit: 100});
             expect(api.get).toHaveBeenCalledTimes(1);
             expect(api.get).toHaveBeenCalledWith(
-                `/rest/organizations/${API.orgPlaceholder}/schema/sources/${sourceType}/entities?clientId=${params.clientId}&instanceUrl=${params.instanceUrl}&clientSecretGuid=${params.clientSecretGuid}&oauthRefreshTokenGuid=${params.oauthRefreshTokenGuid}`
+                `/rest/organizations/${API.orgPlaceholder}/schema/sources/${sourceType}/entities?clientId=${params.clientId}&instanceUrl=${params.instanceUrl}&clientSecretGuid=${params.clientSecretGuid}&oauthRefreshTokenGuid=${params.oauthRefreshTokenGuid}&offset=100&limit=100`
             );
         });
     });
@@ -45,10 +45,10 @@ describe('SchemaService', () => {
     describe('getFields', () => {
         it('should make a GET call to the specific SchemaService url with the correct params', () => {
             const entityName = 'miaowouioui';
-            schemaService.getFields(sourceType, entityName, {...params, offset: 100, limit: 100});
+            schemaService.getFields(sourceType, entityName, params);
             expect(api.get).toHaveBeenCalledTimes(1);
             expect(api.get).toHaveBeenCalledWith(
-                `/rest/organizations/${API.orgPlaceholder}/schema/sources/${sourceType}/entity/${entityName}?clientId=${params.clientId}&instanceUrl=${params.instanceUrl}&clientSecretGuid=${params.clientSecretGuid}&oauthRefreshTokenGuid=${params.oauthRefreshTokenGuid}&offset=100&limit=100`
+                `/rest/organizations/${API.orgPlaceholder}/schema/sources/${sourceType}/entity/${entityName}?clientId=${params.clientId}&instanceUrl=${params.instanceUrl}&clientSecretGuid=${params.clientSecretGuid}&oauthRefreshTokenGuid=${params.oauthRefreshTokenGuid}`
             );
         });
     });
