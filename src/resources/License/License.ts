d6748569b30523781f1f765a316f8ba13fd9f868
Olivier Lamothe <olamothe@coveo.com>
2021-02-19 15:46:13 -0500

    feat(license): support full license API call (#238)
    * feat(license): support full license API call
    
    * feat(license): support full license API call
    
    * feat(license): apply review
    
    Co-authored-by: rosalie-liu <rliu@coveo.com>

--- src/resources/License/License.ts
+++ src/resources/License/License.ts
@@ -10,6 +10,10 @@ export default class License extends Resource {
         return this.api.get<LicenseModel>(sectionName ? `${License.baseUrl}/${sectionName}` : License.baseUrl);
     }
 
+    full() {
+        return this.api.get<LicenseModel>(License.baseUrl);
+    }
+
     update(sectionName: LicenseSection, licenseSection: Record<string, number>) {
         return this.api.put<LicenseModel>(`${License.baseUrl}/${sectionName}`, licenseSection);
     }
