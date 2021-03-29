cd4d9c683cc6d34bb03178c48fc83a438323b582
rosalie-liu <rliu@coveo.com>
2021-03-29 10:24:32 -0400

    feat(exports): add properties descriptions

--- src/resources/UsageAnalytics/Read/Exports/ExportsInterfaces.ts
+++ src/resources/UsageAnalytics/Read/Exports/ExportsInterfaces.ts
@@ -26,22 +26,41 @@ export interface ExportEstimateModel {
 interface EstimateExportCommonParams {
     from: string;
     to: string;
+    /**
+     * The filter that will be applied to the events common dimensions (shared by all types of events).
+     * Multiple filter parameters are joined with the AND operator.
+     */
     f?: string[];
 }
 
 export interface EstimateExportParams extends EstimateExportCommonParams {
     tables?: ExportTablesType[];
+    /**
+     * The filter that will be applied to the click and search events dimensions.
+     * Multiple filter parameters are joined with the AND operator.
+     */
     fs?: string[];
+    /**
+     * The filter that will be applied to the custom events dimensions.
+     * Multiple filter parameters are joined with the AND operator.
+     */
     fc?: string[];
 }
 
 export interface EstimateVisitExportParams extends EstimateExportCommonParams {
     hideEventFilters?: string[];
+    /**
+     * The filter that will be applied to dimensions to exclude events from the results.
+     * Multiple filter parameters are joined with the AND operator.
+     */
     fn?: string[];
 }
 
 interface GenerateExportCommonParams {
     filename?: string;
+    /**
+     * Export's description.
+     */
     d?: string;
     dimensions?: string[];
     format?: CSVFileFormat;
