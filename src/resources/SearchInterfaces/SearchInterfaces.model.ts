cb4b1016f00a838ff167dfef78482c69a62a95c6
Jeremie Robert <jrobert@coveo.com>
2021-04-26 16:27:52 -0400

    chore(search interface): review

--- src/resources/SearchInterfaces/SearchInterfaces.model.ts
+++ src/resources/SearchInterfaces/SearchInterfaces.model.ts
@@ -48,3 +48,25 @@ export interface ISearchInterfaceConfiguration {
      */
     accesses: IAccesses;
 }
+
+export interface IListSearchInterfacesParameters {
+    /**
+     * A substring that must appear in a search interface configuration name for this configuration to appear in results.
+     */
+    filter?: string;
+
+    /**
+     * The sort direction of the results.<br />Possible values:<br /> - `asc`: ascending order<br /> - `desc`: descending order.
+     */
+    order?: 'desc' | 'asc';
+
+    /**
+     * The 0-based number of the page of configurations to list.
+     */
+    page?: number;
+
+    /**
+     * The maximum number of configurations to include per page.
+     */
+    perPage?: number;
+}
