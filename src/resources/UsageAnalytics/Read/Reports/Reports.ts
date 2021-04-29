52a8a94ea36ad8c15830e2c73a9f9cc5cf51524b
Tom Carrio <thomas.carrio@dynatrace.com>
2021-04-29 10:53:53 -0400

    feat(analytics): include complete Reports V15 implementation

--- src/resources/UsageAnalytics/Read/Reports/Reports.ts
+++ src/resources/UsageAnalytics/Read/Reports/Reports.ts
@@ -1,11 +1,19 @@
+import {ReportType} from '../../../Enums';
 import Resource from '../../../Resource';
 import {
     CreateReportModel,
     CreateReportResponse,
     GetReportOptions,
+    GetReportTemplateOptions,
     ListReportsOptions,
     ListReportsResponse,
+    ReportAccessRequest,
+    ReportAccessResponse,
     ReportModel,
+    TemplateResponse,
+    ReportUsersResponse,
+    StatusResponse,
+    TemplateMetadataResponse,
     UpdateReportModel,
 } from './ReportsInterfaces';
 
@@ -25,6 +33,9 @@ export default class Reports extends Resource {
         );
     }
 
+    /**
+     * Get a report
+     */
     get(id: string, options: GetReportOptions = {}) {
         return this.api.get<ReportModel>(
             this.buildPath(`${Reports.baseUrl}/${id}`, {org: this.api.organizationId, tz: options.tz})
@@ -56,5 +67,77 @@ export default class Reports extends Resource {
      */
     delete(id: string) {
         return this.api.delete(this.buildPath(`${Reports.baseUrl}/${id}`, {org: this.api.organizationId}));
+    }
+
+    /**
+     * Get the users and groups who can view a report
+     */
+    getAccess(reportId: string) {
+        return this.api.get<ReportAccessResponse>(
+            this.buildPath(`${Reports.baseUrl}/${reportId}/access`, {org: this.api.organizationId})
+        );
+    }
+
+    /**
+     * Set the users and groups who are allowed to view a report
+     */
+    setAccess(reportId: string, reportAccess: ReportAccessRequest) {
+        return this.api.put(
+            this.buildPath(`${Reports.baseUrl}/${reportId}/access`, {org: this.api.organizationId}),
+            reportAccess
+        );
+    }
+
+    /**
+     * Get the users who can view a report
+     */
+    getUsers(reportId: string) {
+        return this.api.get<ReportUsersResponse>(
+            this.buildPath(`${Reports.baseUrl}/${reportId}/users`, {org: this.api.organizationId})
+        );
+    }
+
+    /**
+     * Set the users who are allowed to view a report
+     */
+    setUsers(reportId: string, userIds: string[]) {
+        return this.api.put(
+            this.buildPath(`${Reports.baseUrl}/${reportId}/users`, {org: this.api.organizationId}),
+            userIds
+        );
+    }
+
+    /**
+     * Health check for the reports service
+     */
+    healthcheck() {
+        return this.api.get<StatusResponse>(
+            this.buildPath(`${Reports.baseUrl}/monitoring/health`, {org: this.api.organizationId})
+        );
+    }
+
+    /**
+     * Get a report template
+     */
+    getReportTemplate(templateId: string, options: GetReportTemplateOptions = {}) {
+        return this.api.get<TemplateResponse[]>(this.buildPath(`${Reports.baseUrl}/templates/${templateId}`, options));
+    }
+
+    /**
+     * Get metadata about available report templates
+     */
+    listReportTemplates(type: ReportType, options: GetReportTemplateOptions = {}) {
+        return this.api.get<TemplateMetadataResponse[]>(
+            this.buildPath(`${Reports.baseUrl}/templates`, {...options, type})
+        );
+    }
+
+    /**
+     * Get the reports service status
+     */
+    getServiceStatus() {
+        return this.api.get<StatusResponse>(
+            this.buildPath(`${Reports.baseUrl}/status`, {org: this.api.organizationId})
+        );
     }
 }
