b882559f393f3814396424745960ca791781608c
Tom <thomas.carrio@dynatrace.com>
2021-06-08 10:43:50 -0400

    feat(filters): support for permission filters (#329)

--- src/resources/UsageAnalytics/Read/Filters/Filters.ts
+++ src/resources/UsageAnalytics/Read/Filters/Filters.ts
@@ -1,9 +1,19 @@
 import Resource from '../../../Resource';
 import {
+    CreatePermissionsFilterModel,
+    CreatePermissionsFilterResponse,
     CreateReportingFilterModel,
     CreateReportingFilterResponse,
     FilterModel,
+    FilterResponse,
+    FilterServiceHealthResponse,
+    FilterServiceStatusResponse,
+    FilterTargetsModel,
+    FilterTargetsResponse,
     ListFiltersResponse,
+    PermissionsFilterUser,
+    UpdatePermissionsFilterModel,
+    UpdatePermissionsFilterResponse,
     UpdateReportingFilterModel,
     UpdateReportingFilterResponse,
 } from './FiltersInterfaces';
@@ -11,6 +21,7 @@ import {
 export default class Filters extends Resource {
     static baseUrl = '/rest/ua/v15/filters';
     static reportingBaseUrl = `${Filters.baseUrl}/reporting`;
+    static permissionsBaseUrl = `${Filters.baseUrl}/permissions`;
 
     /**
      * Get all reporting filters
@@ -25,7 +36,7 @@ export default class Filters extends Resource {
      * Get a reporting filter
      */
     getReportFilter(id: string) {
-        return this.api.get<FilterModel>(
+        return this.api.get<FilterResponse>(
             this.buildPath(`${Filters.reportingBaseUrl}/${id}`, {org: this.api.organizationId})
         );
     }
@@ -35,7 +46,7 @@ export default class Filters extends Resource {
      */
     createReportFilter(filter: CreateReportingFilterModel) {
         return this.api.post<CreateReportingFilterResponse>(
-            this.buildPath(`${Filters.reportingBaseUrl}`, {org: this.api.organizationId}),
+            this.buildPath(Filters.reportingBaseUrl, {org: this.api.organizationId}),
             filter
         );
     }
@@ -55,5 +66,98 @@ export default class Filters extends Resource {
      */
     deleteReportFilter(id: string) {
         return this.api.delete(this.buildPath(`${Filters.reportingBaseUrl}/${id}`, {org: this.api.organizationId}));
+    }
+
+    /**
+     * Get all permission filters
+     */
+    listPermissionFilters() {
+        return this.api.get<ListFiltersResponse>(
+            this.buildPath(Filters.permissionsBaseUrl, {org: this.api.organizationId})
+        );
+    }
+
+    /**
+     * Get a permission filter
+     */
+    getPermissionFilter(id: string) {
+        return this.api.get<FilterResponse>(
+            this.buildPath(`${Filters.permissionsBaseUrl}/${id}`, {org: this.api.organizationId})
+        );
+    }
+
+    /**
+     * Create a permission filter
+     */
+    createPermissionFilter(filter: CreatePermissionsFilterModel) {
+        return this.api.post<CreatePermissionsFilterResponse>(
+            this.buildPath(Filters.permissionsBaseUrl, {org: this.api.organizationId}),
+            filter
+        );
+    }
+
+    /**
+     * Update a permission filter
+     */
+    updatePermissionFilter(id: string, filter: UpdatePermissionsFilterModel) {
+        return this.api.put<UpdatePermissionsFilterResponse>(
+            this.buildPath(`${Filters.permissionsBaseUrl}/${id}`, {org: this.api.organizationId}),
+            filter
+        );
+    }
+
+    /**
+     * Delete a permission filter
+     */
+    deletePermissionFilter(id: string) {
+        return this.api.delete(this.buildPath(`${Filters.permissionsBaseUrl}/${id}`, {org: this.api.organizationId}));
+    }
+
+    /**
+     * Get the users of a permission filters
+     */
+    getPermissionFilterUsers(id: string) {
+        return this.api.get<PermissionsFilterUser[]>(this.buildPath(`${Filters.permissionsBaseUrl}/${id}/users`, {}));
+    }
+
+    /**
+     * Set the users of a permission filter
+     */
+    updatePermissionFilterUsers(id: string, users: string[]) {
+        return this.api.put(
+            this.buildPath(`${Filters.permissionsBaseUrl}/${id}/users`, {org: this.api.organizationId}),
+            users
+        );
+    }
+
+    /**
+     * Get the targets of a permission filters
+     */
+    getPermissionFilterTargets(id: string) {
+        return this.api.get<PermissionsFilterUser[]>(this.buildPath(`${Filters.permissionsBaseUrl}/${id}/targets`, {}));
+    }
+
+    /**
+     * Set the targets of a permission filter
+     */
+    updatePermissionFilterTargets(id: string, targets: FilterTargetsModel) {
+        return this.api.put<FilterTargetsResponse>(
+            this.buildPath(`${Filters.permissionsBaseUrl}/${id}/targets`, {org: this.api.organizationId}),
+            targets
+        );
+    }
+
+    /**
+     * Health check for the filter service
+     */
+    healthcheck() {
+        return this.api.get<FilterServiceHealthResponse>(`${Filters.baseUrl}/monitoring/health`);
+    }
+
+    /**
+     * Get the filter service status
+     */
+    status() {
+        return this.api.get<FilterServiceStatusResponse>(`${Filters.baseUrl}/status`);
     }
 }
--- src/resources/UsageAnalytics/Read/Filters/FiltersInterfaces.ts
+++ src/resources/UsageAnalytics/Read/Filters/FiltersInterfaces.ts
@@ -1,3 +1,5 @@
+import {GroupResponse, UserResponse} from '../Reports';
+
 export interface FilterModel {
     /** The display name of the filter. */
     displayName: string;
@@ -29,7 +31,61 @@ export type CreateReportingFilterResponse = Pick<FilterResponse, 'id'>;
 export type UpdateReportingFilterModel = CreateReportingFilterModel;
 export type UpdateReportingFilterResponse = CreateReportingFilterResponse;
 
+export type CreatePermissionsFilterModel = FilterModel;
+export type CreatePermissionsFilterResponse = Pick<FilterResponse, 'id'>;
+
+export type UpdatePermissionsFilterModel = CreatePermissionsFilterModel;
+export type UpdatePermissionsFilterResponse = CreatePermissionsFilterResponse;
+
 export interface ListFiltersResponse {
     /** A list of filters */
     filters: FilterModel[];
+}
+
+export interface PermissionsFilterUser {
+    /** The user id. */
+    userId: string;
+
+    /** The account. */
+    account: string;
+
+    /** Permissions for the user */
+    permissions: PermissionsFilterUserPermissions[];
+
+    /** Related reports */
+    reportIds: unknown[];
+}
+
+export interface PermissionsFilterUserPermissions {
+    /** The permission filter. */
+    permission: string;
+
+    /** Whether the permission field is the unique ID. */
+    filterId: boolean;
+}
+
+export interface FilterTargetsModel {
+    /** The ids of the users targeted by the filter. */
+    targetedUsers: string[];
+
+    /** The ids of the groups targeted by the filter. */
+    targetedGroups: string[];
+}
+
+export interface FilterTargetsResponse {
+    /** The users targeted by the filter. */
+    targetedUsers: UserResponse[];
+
+    /** The groups targeted by the filter. */
+    targetedGroups: GroupResponse[];
+}
+
+export interface FilterServiceHealthResponse {
+    /** The health of the service */
+    status: 'UP' | 'DOWN';
+}
+
+export interface FilterServiceStatusResponse {
+    /** The status of the service */
+    status: 'online' | 'offline';
 }
