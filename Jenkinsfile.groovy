3958bb7411750a750866c2466402450bd4760988
Prou Yann <yprou@coveo.com>
2021-11-05 16:55:05 -0400

    ci: fix create release

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -21,10 +21,10 @@ pipeline {
 
   environment {
     NPM_TOKEN = credentials("npmjs_com_token")
-    GIT = credentials("github-commit-token")
-    GH_TOKEN = credentials("github-coveobot")
-
+    GIT = credentials("github-coveobot")
+    GH_TOKEN = credentials("github-commit-token")
   }
+
   options {
     ansiColor("xterm")
     timestamps()
@@ -46,12 +46,12 @@ pipeline {
             $class: 'GitSCM',
             branches: scm.branches,
             extensions: scm.extensions + [[$class: "CleanCheckout"]] + [[$class: "LocalBranch", localBranch: "**"]] + [[$class: 'CloneOption', noTags: false, reference: '', shallow: false]],
-            userRemoteConfigs: [[credentialsId: "github-app-dev", url: "https://github.com/coveo/platform-client.git"]]
+            userRemoteConfigs: [[credentialsId: "github-coveobot", url: "https://github.com/coveo/platform-client.git"]]
           ])
 
           sh "git config --global user.email \"jenkins@coveo.com\""
           sh "git config --global user.name \"Jenkins CI\""
-          sh "git remote set-url origin \"https://${env.GIT_USR}:${env.GIT_PSW}@github.com/coveo/platform-client.git\""
+          sh "git remote set-url origin \"https://${env.GH_TOKEN}@github.com/coveo/platform-client.git\""
 
           def nodeHome = tool name: env.BUILD_NODE_VERSION, type: "nodejs"
           env.PATH = "${nodeHome}/bin:${env.PATH}"
@@ -93,7 +93,7 @@ pipeline {
 
     stage('Publish') {
       when {
-        expression { env.BRANCH_NAME == 'master' }
+        expression { env.BRANCH_NAME ==~ /(master|fix\/create-release)/ }
       }
       
       steps {
