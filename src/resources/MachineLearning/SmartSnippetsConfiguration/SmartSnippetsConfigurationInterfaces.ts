5bee63188d682280b4653d8fe6d1bd581e05f5d0
nlafrance-coveo <nlafrance@coveo.com>
2021-12-08 16:39:20 -0500

    feat(mlconfig): added interface descriptions

--- src/resources/MachineLearning/SmartSnippetsConfiguration/SmartSnippetsConfigurationInterfaces.ts
+++ src/resources/MachineLearning/SmartSnippetsConfiguration/SmartSnippetsConfigurationInterfaces.ts
@@ -36,40 +36,85 @@ export interface SmartSnippetsConfigurationModel {
 export type DocumentRequirementStatus = 'OK' | 'INSUFFICIENT_DOCUMENTS';
 
 export interface SmartSnippetsDocumentGroupPreviewParams {
+    /**
+     * The sources to consider.
+     */
     sources: string[];
 }
 
 export interface SmartSnippetsDocumentGroupPreview {
+    /**
+     * The query that was used to fetch document information.
+     */
     query: string;
+    /**
+     * Status indicating whether there are enough candidates for learning.
+     */
     documentRequirementStatus: DocumentRequirementStatus;
+    /**
+     * The total number of documents in all sources.
+     */
     numberOfDocuments: number;
+    /**
+     * The number of documents that have a permanent id.
+     */
     numberOfDocumentsWithPermanentId: number;
+    /**
+     * The number of documents that are candidates for learning.
+     */
     numberOfValidDocuments: number;
 }
 
 export interface SmartSnippetsContentFieldsParams {
+    /**
+     * The type of documents to target.
+     */
     documentType: string;
+    /**
+     * The names of the sources to consider.
+     */
     sources: string[];
 }
 
 export interface SmartSnippetsContentField {
+    /**
+     * The name of the field.
+     */
     name: string;
 }
 
 export interface SmartSnippetsContentFields {
+    /**
+     * The query that was used to fetch document information.
+     */
     query: string;
+    /**
+     * The list of fields that are non-empty for at least one document.
+     */
     fields: SmartSnippetsContentField[];
 }
 
 export interface SmartSnippetsDocumentTypesParams {
+    /**
+     * The sources to use to consider.
+     */
     sources: string[];
 }
 
 export interface SmartSnippetsDocumentType {
+    /**
+     * The document type.
+     */
     documentType: string;
 }
 
 export interface SmartSnippetsDocumentTypes {
+    /**
+     * The query that was used to fetch document information.
+     */
     query: string;
+    /**
+     * The document types in all sources.
+     */
     documentTypes: SmartSnippetsDocumentType[];
 }
