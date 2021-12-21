a159735e75113b775fd34f57d7d30e1a4263f7dc
toofff <yprou@coveo.com>
2021-12-21 22:34:06 +0100

    Revert "feat(ml): added and updated Case Classification ML Config api calls (#404)" (#406)
    This reverts commit 69e2b1ebc196d8bc7409c377f9305031d425ce10.

--- src/resources/MachineLearning/CaseClassificationConfiguration/CaseClassificationConfiguration.ts
+++ src/resources/MachineLearning/CaseClassificationConfiguration/CaseClassificationConfiguration.ts
@@ -1,52 +1,32 @@
 import API from '../../../APICore';
 import {New} from '../../BaseInterfaces';
 import Resource from '../../Resource';
-import {
-    CaseClassificationConfigurationModel,
-    CaseClassificationContentFields,
-    CaseClassificationContentFieldsParams,
-    CaseClassificationDocumentGroupPreview,
-    CaseClassificationDocumentGroupPreviewParams,
-} from './CaseClassificationConfigurationInterfaces';
+import {CaseClassificationConfigurationModel} from './CaseClassificationConfigurationInterfaces';
 
 export default class CaseClassificationConfiguration extends Resource {
-    static baseUrl = `/rest/organizations/${API.orgPlaceholder}/machinelearning/configuration/caseclassif`;
-    static modelUrl = `${CaseClassificationConfiguration.baseUrl}/model`;
-    static fieldsUrl = `${CaseClassificationConfiguration.baseUrl}/fields`;
-    static previewUrl = `${CaseClassificationConfiguration.baseUrl}/preview`;
+    static baseUrl = `/rest/organizations/${API.orgPlaceholder}/machinelearning/configuration/caseclassif/model`;
 
     create(configModel: New<CaseClassificationConfigurationModel, 'modelId'>) {
         return this.api.post<CaseClassificationConfigurationModel>(
-            CaseClassificationConfiguration.modelUrl,
+            CaseClassificationConfiguration.baseUrl,
             configModel
         );
     }
 
     delete(modelId: string) {
-        return this.api.delete(`${CaseClassificationConfiguration.modelUrl}/${modelId}`);
+        return this.api.delete(`${CaseClassificationConfiguration.baseUrl}/${modelId}`);
     }
 
     get(modelId: string) {
         return this.api.get<CaseClassificationConfigurationModel>(
-            `${CaseClassificationConfiguration.modelUrl}/${modelId}`
+            `${CaseClassificationConfiguration.baseUrl}/${modelId}`
         );
     }
 
     update(configModel: CaseClassificationConfigurationModel) {
         return this.api.put<CaseClassificationConfigurationModel>(
-            `${CaseClassificationConfiguration.modelUrl}/${configModel.modelId}`,
+            `${CaseClassificationConfiguration.baseUrl}/${configModel.modelId}`,
             configModel
-        );
-    }
-
-    fields(params: CaseClassificationContentFieldsParams) {
-        return this.api.post<CaseClassificationContentFields>(CaseClassificationConfiguration.fieldsUrl, params);
-    }
-
-    preview(params: CaseClassificationDocumentGroupPreviewParams) {
-        return this.api.post<CaseClassificationDocumentGroupPreview>(
-            CaseClassificationConfiguration.previewUrl,
-            params
         );
     }
 }
--- src/resources/MachineLearning/CaseClassificationConfiguration/CaseClassificationConfigurationInterfaces.ts
+++ src/resources/MachineLearning/CaseClassificationConfiguration/CaseClassificationConfigurationInterfaces.ts
@@ -3,8 +3,6 @@ export enum Operator {
     NotEquals = 'NOT_EQUALS',
 }
 
-export type CaseClassificationDocumentRequirementStatus = 'OK' | 'INSUFFICIENT_DOCUMENTS';
-
 export interface FilterConditions {
     /**
      * The name of the field.
@@ -23,36 +21,7 @@ export interface FilterConditions {
     value: string;
 }
 
-export interface DateField {
-    /**
-     * The field to use as date.
-     */
-    dateField?: string;
-}
-
-export interface FixedExtractionPeriod {
-    /**
-     * Date in milliseconds that indicates the beginning of the time interval to include cases.
-     * The date format is the difference, measured in milliseconds, between the current time and midnight, January 1, 1970 UTC.
-     */
-    startTime: number;
-    /**
-     * Date in milliseconds that indicates the end of the time interval to include cases.
-     * The date format is the difference, measured in milliseconds, between the current time and midnight, January 1, 1970 UTC.
-     */
-    endTime: number;
-}
-
-export interface IntervalExtractionPeriod {
-    /**
-     * The period to export data from. The exportPeriod uses the moment when the model was generated as a base.
-     * Must be in the ISO8601 period format (i.e., PyYmMwWdDThHmMsS).
-     * Example: P3Y6M4DT12H30M5S Represents a duration of three years, six months, four days, twelve hours, thirty minutes, and five seconds.
-     */
-    exportPeriod: string;
-}
-
-export type ExtractionPeriod = DateField & (FixedExtractionPeriod | IntervalExtractionPeriod);
+export type ExtractionPeriod = FixedExtractionPeriod | IntervalExtractionPeriod;
 
 export interface CaseClassificationConfigurationModel {
     /**
@@ -92,84 +61,26 @@ export interface CaseClassificationConfigurationModel {
      * Example: [subject, reason, category]
      */
     fieldsToPredict: string[];
-    /**
-     * The field value to use as language.
-     */
-    languageField?: string;
 }
 
-export interface CaseClassificationDocumentGroupPreviewParams {
+export interface FixedExtractionPeriod {
     /**
-     * The time period for which to extract cases for model building.
-     * Must contain an export period or a start time and end time.
+     * Date in milliseconds that indicates the beginning of the time interval to include cases.
+     * The date format is the difference, measured in milliseconds, between the current time and midnight, January 1, 1970 UTC.
      */
-    caseExtractionPeriod: ExtractionPeriod;
+    startTime: number;
     /**
-     * An array of filtering conditions.
+     * Date in milliseconds that indicates the end of the time interval to include cases.
+     * The date format is the difference, measured in milliseconds, between the current time and midnight, January 1, 1970 UTC.
      */
-    caseFilterConditions: FilterConditions[];
-    /**
-     * The field value to use as language.
-     */
-    languageField: string;
-    /**
-     * The names of the sources containing the cases to use for model building.
-     */
-    sources: string[];
+    endTime: number;
 }
 
-export interface CaseClassificationDocumentGroupPreview {
+export interface IntervalExtractionPeriod {
     /**
-     * The query that was used to fetch document information.
+     * The period to export data from. The exportPeriod uses the moment when the model was generated as a base.
+     * Must be in the ISO8601 period format (i.e., PyYmMwWdDThHmMsS).
+     * Example: P3Y6M4DT12H30M5S Represents a duration of three years, six months, four days, twelve hours, thirty minutes, and five seconds.
      */
-    query: string;
-    /**
-     * Status indicating whether there are enough candidates for learning.
-     */
-    documentRequirementStatus: CaseClassificationDocumentRequirementStatus;
-    /**
-     * The total number of documents in all sources.
-     */
-    numberOfDocuments: number;
-    /**
-     * The number of documents matching the configured conditions.
-     */
-    numberOfDocumentsMatchingConditions: number;
-    /**
-     * The number of English documents matching the configured conditions.
-     */
-    numberOfDocumentsMatchingConditionsAndInEnglish: number;
-    /**
-     * The number of documents that are candidates for learning.
-     */
-    numberOfValidDocuments: number;
-}
-
-export interface CaseClassificationContentFieldsParams {
-    /**
-     * The time period for which to extract cases for model building.
-     * Must contain an export period or a start time and end time.
-     */
-    caseExtractionPeriod: ExtractionPeriod;
-    /**
-     * An array of filtering conditions.
-     */
-    caseFilterConditions: FilterConditions[];
-    /**
-     * The field value to use as language.
-     */
-    languageField: string;
-    /**
-     * The names of the sources containing the cases to use for model building.
-     */
-    sources: string[];
-}
-
-export interface CaseClassificationContentField {
-    name: string;
-}
-
-export interface CaseClassificationContentFields {
-    fields: CaseClassificationContentField[];
-    query: string;
+    exportPeriod: string;
 }
