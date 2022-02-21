c31e75561ba7a30548b995b1af32665c51cc9e96
Felix <fblais@coveo.com>
2022-02-21 12:26:33 -0500

    feat(datashare): +API calls for data share feature

---
+++ src/resources/UsageAnalytics/Read/DataShare/DataShare.ts
@@ -0,0 +1,34 @@
+import Resource from '../../../Resource';
+import {
+    SnowflakeAccountDataSharingParams,
+    SnowflakeAccountDataSharingAccountModel,
+    SnowflakeAccountDataSharingRegionsModel,
+} from './DataShareInterfaces';
+
+export default class DataShare extends Resource {
+    static baseUrl = '/rest/ua/v15/admin/snowflake/securedatasharing';
+
+    listSnowflakeAccount() {
+        return this.api.get<SnowflakeAccountDataSharingAccountModel[]>(
+            this.buildPath(`${DataShare.baseUrl}/accounts`, {org: this.api.organizationId})
+        );
+    }
+
+    addSnowflakeAccount(params: SnowflakeAccountDataSharingParams) {
+        return this.api.post<void>(
+            this.buildPath(`${DataShare.baseUrl}/accounts`, {...params, org: this.api.organizationId})
+        );
+    }
+
+    deleteSnowflakeAccount(params: SnowflakeAccountDataSharingParams) {
+        return this.api.delete<void>(
+            this.buildPath(`${DataShare.baseUrl}/accounts`, {...params, org: this.api.organizationId})
+        );
+    }
+
+    listAllowedSnowflakeRegions() {
+        return this.api.get<SnowflakeAccountDataSharingRegionsModel>(
+            this.buildPath(`${DataShare.baseUrl}/regions`, null)
+        );
+    }
+}
---
+++ src/resources/UsageAnalytics/Read/DataShare/tests/DataShare.spec.ts
@@ -0,0 +1,57 @@
+import API from '../../../../../APICore';
+import DataShare from '../DataShare';
+
+jest.mock('../../../../../APICore');
+
+const APIMock: jest.Mock<API> = API as any;
+
+describe('Dimensions', () => {
+    let dataShare: DataShare;
+    const dataShareParams = {
+        accountLocator: 'Soubane',
+        snowflakeRegion: 'LOUBAME',
+    };
+    const api = new APIMock() as jest.Mocked<API>;
+    const serverlessApi = new APIMock() as jest.Mocked<API>;
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+        dataShare = new DataShare(api, serverlessApi);
+    });
+
+    describe('listSnowflakeAccount', () => {
+        it('should make a GET call to the snowflake data share accounts url', () => {
+            dataShare.listSnowflakeAccount();
+            expect(api.get).toHaveBeenCalledTimes(1);
+            expect(api.get).toHaveBeenCalledWith(`${DataShare.baseUrl}/accounts`);
+        });
+    });
+
+    describe('addSnowflakeAccount', () => {
+        it('should make a POST call to the snowflake data share accounts url', () => {
+            dataShare.addSnowflakeAccount(dataShareParams);
+            expect(api.post).toHaveBeenCalledTimes(1);
+            expect(api.post).toHaveBeenCalledWith(
+                `${DataShare.baseUrl}/accounts?accountLocator=Soubane&snowflakeRegion=LOUBAME`
+            );
+        });
+    });
+
+    describe('deleteSnowflakeAccount', () => {
+        it('should make a DELETE call to the snowflake data share accounts url', () => {
+            dataShare.deleteSnowflakeAccount(dataShareParams);
+            expect(api.delete).toHaveBeenCalledTimes(1);
+            expect(api.delete).toHaveBeenCalledWith(
+                `${DataShare.baseUrl}/accounts?accountLocator=Soubane&snowflakeRegion=LOUBAME`
+            );
+        });
+    });
+
+    describe('listAllowedSnowflakeRegions', () => {
+        it('should make a GET call to the snowflake data share regions url', () => {
+            dataShare.listAllowedSnowflakeRegions();
+            expect(api.get).toHaveBeenCalledTimes(1);
+            expect(api.get).toHaveBeenCalledWith(`${DataShare.baseUrl}/regions`);
+        });
+    });
+});
