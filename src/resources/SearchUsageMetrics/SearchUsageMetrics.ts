e3c3765317b7baf7efd7cbf07cf3067ee6ade03d
Sylvie Allain <sallain@coveo.com>
2022-02-24 19:43:34 -0500

    feat(raw-metrics): add interfaces, calls and enums

--- src/resources/SearchUsageMetrics/SearchUsageMetrics.ts
+++ src/resources/SearchUsageMetrics/SearchUsageMetrics.ts
@@ -1,16 +1,19 @@
 import API from '../../APICore';
 import Resource from '../Resource';
 import LicenseMetrics from './LicenseMetrics/LicenseMetrics';
+import RawMetrics from './RawMetrics/RawMetrics';
 import SearchHubs from './SearchHubs/SearchHubs';
 
 export default class SearchUsageMetrics extends Resource {
     licenseMetrics: LicenseMetrics;
     searchHubs: SearchHubs;
+    rawMetrics: RawMetrics;
 
     constructor(protected api: API, protected serverlessApi: API) {
         super(api, serverlessApi);
 
         this.licenseMetrics = new LicenseMetrics(api, serverlessApi);
         this.searchHubs = new SearchHubs(api, serverlessApi);
+        this.rawMetrics = new RawMetrics(api, serverlessApi);
     }
 }
