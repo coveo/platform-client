e26824c1681c4aed4a11d0773a061c140a5fd977
Luc Bergeron <10946843+lbergeron@users.noreply.github.com>
2022-03-28 15:11:34 -0400

    feat(service): add insight panel config resource (#435)
    https://coveord.atlassian.net/browse/SVCC-459

---
+++ src/resources/InsightPanelConfigs/InsightPanelConfig.ts
@@ -0,0 +1,34 @@
+import API from '../../APICore';
+import {New, PageModel} from '../BaseInterfaces';
+import Resource from '../Resource';
+import {
+    InsightPanelConfigCreationParams,
+    InsightPanelConfigListOptions,
+    InsightPanelConfigModel,
+} from './InsightPanelConfigInterfaces';
+
+export default class InsightPanelConfig extends Resource {
+    static baseUrl = `/rest/organizations/${API.orgPlaceholder}/insightconfig/v1/configs`;
+
+    list(options?: InsightPanelConfigListOptions) {
+        return this.api.get<PageModel<InsightPanelConfigModel>>(this.buildPath(InsightPanelConfig.baseUrl, options));
+    }
+
+    create(insightPanelConfig: New<InsightPanelConfigCreationParams>) {
+        return this.api.post<InsightPanelConfigModel>(InsightPanelConfig.baseUrl, insightPanelConfig);
+    }
+
+    delete(insightPanelConfigId: string) {
+        return this.api.delete(`${InsightPanelConfig.baseUrl}/${insightPanelConfigId}`);
+    }
+
+    get(insightPanelConfigId: string) {
+        return this.api.get<InsightPanelConfigModel>(`${InsightPanelConfig.baseUrl}/${insightPanelConfigId}`);
+    }
+
+    update(insightPanelConfig: InsightPanelConfigCreationParams) {
+        const {id, ...body} = insightPanelConfig;
+
+        return this.api.put<InsightPanelConfigModel>(`${InsightPanelConfig.baseUrl}/${id}`, body);
+    }
+}
