c38a09ca6b74b4b6eded091e2f6727f93353ca90
Louis Bompart <lbompart@coveo.com>
2022-04-05 12:27:06 -0400

    test(datashare): improve test

--- src/resources/UsageAnalytics/Read/DataShare/tests/DataShare.spec.ts
+++ src/resources/UsageAnalytics/Read/DataShare/tests/DataShare.spec.ts
@@ -3,27 +3,24 @@ import DataShare from '../DataShare';
 
 jest.mock('../../../../../APICore');
 
-const APIMock: jest.Mock<API> = API as any;
-
 describe('Dimensions', () => {
     let dataShare: DataShare;
     const dataShareParams = {
         accountLocator: 'Soubane',
         snowflakeRegion: 'LOUBAME',
     };
-    const api = new APIMock() as jest.Mocked<API>;
-    const serverlessApi = new APIMock() as jest.Mocked<API>;
-
+    const api = jest.mocked(new API(null, null), true);
+    Object.defineProperty(api, 'organizationId', {get: () => 'someOrgId'});
     beforeEach(() => {
         jest.clearAllMocks();
-        dataShare = new DataShare(api, serverlessApi);
+        dataShare = new DataShare(api, null);
     });
 
     describe('listSnowflakeAccount', () => {
         it('should make a GET call to the snowflake data share accounts url', () => {
             dataShare.listSnowflakeAccount();
             expect(api.get).toHaveBeenCalledTimes(1);
-            expect(api.get).toHaveBeenCalledWith(`${DataShare.baseUrl}/accounts`);
+            expect(api.get).toHaveBeenCalledWith(`${DataShare.baseUrl}/accounts?org=someOrgId`);
         });
     });
 
@@ -31,7 +28,7 @@ describe('Dimensions', () => {
         it('should make a POST call to the snowflake data share accounts url', () => {
             dataShare.addSnowflakeAccount(dataShareParams);
             expect(api.post).toHaveBeenCalledTimes(1);
-            expect(api.post).toHaveBeenCalledWith(`${DataShare.baseUrl}/accounts`, dataShareParams);
+            expect(api.post).toHaveBeenCalledWith(`${DataShare.baseUrl}/accounts?org=someOrgId`, dataShareParams);
         });
     });
 
@@ -40,7 +37,7 @@ describe('Dimensions', () => {
             dataShare.deleteSnowflakeAccount(dataShareParams);
             expect(api.delete).toHaveBeenCalledTimes(1);
             expect(api.delete).toHaveBeenCalledWith(
-                `${DataShare.baseUrl}/accounts?accountLocator=Soubane&snowflakeRegion=LOUBAME`
+                `${DataShare.baseUrl}/accounts?accountLocator=Soubane&snowflakeRegion=LOUBAME&org=someOrgId`
             );
         });
     });
@@ -49,7 +46,7 @@ describe('Dimensions', () => {
         it('should make a GET call to the snowflake data share regions url', () => {
             dataShare.listAllowedSnowflakeRegions();
             expect(api.get).toHaveBeenCalledTimes(1);
-            expect(api.get).toHaveBeenCalledWith(`${DataShare.baseUrl}/regions`);
+            expect(api.get).toHaveBeenCalledWith(`${DataShare.baseUrl}/regions?org=someOrgId`);
         });
     });
 });
