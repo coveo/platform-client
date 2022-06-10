a706d6f0e8bbaf152d5ec0d05228e809e3093ef3
moganon <mogagnon@coveo.com>
2022-06-10 14:45:47 -0400

    feat(metadata): add doc and PageModel [CTINFRA-2754]

--- src/resources/Sources/SourcesMetadata/SourcesMetadata.ts
+++ src/resources/Sources/SourcesMetadata/SourcesMetadata.ts
@@ -1,12 +1,14 @@
 import Resource from '../../Resource';
 import API from '../../../APICore';
-import {Paginated} from '../../BaseInterfaces';
+import {PageModel, Paginated} from '../../BaseInterfaces';
 import {Metadata} from './SourcesMetadataInterfaces';
 
 export default class SourcesMetadata extends Resource {
     static baseUrl = `/rest/organizations/${API.orgPlaceholder}/sources`;
 
     list(sourceId: string, params?: Paginated) {
-        return this.api.get<Metadata[]>(this.buildPath(`${SourcesMetadata.baseUrl}/${sourceId}/metadata`, params));
+        return this.api.get<PageModel<Metadata>>(
+            this.buildPath(`${SourcesMetadata.baseUrl}/${sourceId}/metadata`, params)
+        );
     }
 }
--- src/resources/Sources/SourcesMetadata/SourcesMetadataInterfaces.ts
+++ src/resources/Sources/SourcesMetadata/SourcesMetadataInterfaces.ts
@@ -1,6 +1,18 @@
 export interface Metadata {
+    /**
+     * The key for a metadata.
+     */
     key: string;
+    /**
+     * The frequency at which this metadata has been seen on documents.
+     */
     frequency: number;
+    /**
+     * Subset of values for the metadata.
+     */
     values: any[];
+    /**
+     * The origin of the metadata.
+     */
     origin: string;
 }
