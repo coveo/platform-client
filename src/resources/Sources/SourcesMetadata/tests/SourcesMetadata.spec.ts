f2eea713320ff4f3c5cedf5cdc4a0cd26fbef0b6
moganon <mogagnon@coveo.com>
2022-06-13 14:28:58 -0400

    feat(metadata): fix test [CTINFRA-2754]

--- src/resources/Sources/SourcesMetadata/tests/SourcesMetadata.spec.ts
+++ src/resources/Sources/SourcesMetadata/tests/SourcesMetadata.spec.ts
@@ -15,13 +15,23 @@ describe('SourcesMetadata', () => {
         metadata = new SourcesMetadata(api, serverlessApi);
     });
 
-    describe('list', () => {
+    describe('getReport', () => {
         it('should make a GET call to the specific SourceMetadata url', () => {
             const sourceId = 'ben&flo';
 
-            metadata.list(sourceId);
+            metadata.getReport(sourceId);
             expect(api.get).toHaveBeenCalledTimes(1);
             expect(api.get).toHaveBeenCalledWith(`/rest/organizations/{organizationName}/sources/${sourceId}/metadata`);
+        });
+    });
+
+    describe('generateRepost', () => {
+        it('should make a POST call to the specific SourceMetadata url', () => {
+            const sourceId = 'ben&flo';
+
+            metadata.generateReport(sourceId);
+            expect(api.post).toHaveBeenCalledTimes(1);
+            expect(api.post).toHaveBeenCalledWith(`/rest/organizations/{organizationName}/sources/${sourceId}/metadata`);
         });
     });
 });
