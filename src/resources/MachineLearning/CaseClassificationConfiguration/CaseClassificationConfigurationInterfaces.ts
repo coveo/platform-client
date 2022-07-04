23583794f98c93fa240b5a7b9cb2f7145a22200d
Martin Laporte <mlaporte@coveo.com>
2022-07-04 10:01:08 +0200

    Remove unneeded enum.
    # Conflicts:
    #	src/resources/MachineLearning/CaseClassificationConfiguration/CaseClassificationConfigurationInterfaces.ts
    https://coveord.atlassian.net/browse/MLX-86

--- src/resources/MachineLearning/CaseClassificationConfiguration/CaseClassificationConfigurationInterfaces.ts
+++ src/resources/MachineLearning/CaseClassificationConfiguration/CaseClassificationConfigurationInterfaces.ts
@@ -1,6 +1,22 @@
-import {FilterConditions} from '../FilterConditions';
-
 export type CaseClassificationDocumentRequirementStatus = 'OK' | 'INSUFFICIENT_DOCUMENTS';
+
+export interface FilterConditions {
+    /**
+     * The name of the field.
+     * Example: status
+     */
+    field: string;
+    /**
+     * The operator to use to evaluate the condition.
+     * Must be EQUALS or NOT_EQUALS
+     */
+    operator: 'EQUALS' | 'NOT_EQUALS';
+    /**
+     * The value to use for the condition.
+     * Example: completed
+     */
+    value: string;
+}
 
 export interface DateField {
     /**
