946e55dd6abafaca9cbee4f455ad69aaaf5863d2
Martin Laporte <mlaporte@coveo.com>
2022-07-04 10:14:49 +0200

    Fix bad merge. https://coveord.atlassian.net/browse/MLX-86

--- src/resources/MachineLearning/CaseClassificationConfiguration/CaseClassificationConfigurationInterfaces.ts
+++ src/resources/MachineLearning/CaseClassificationConfiguration/CaseClassificationConfigurationInterfaces.ts
@@ -1,22 +1,6 @@
-export type CaseClassificationDocumentRequirementStatus = 'OK' | 'INSUFFICIENT_DOCUMENTS';
+import {FilterConditions} from '../FilterConditions';
 
-export interface FilterConditions {
-    /**
-     * The name of the field.
-     * Example: status
-     */
-    field: string;
-    /**
-     * The operator to use to evaluate the condition.
-     * Must be EQUALS or NOT_EQUALS
-     */
-    operator: 'EQUALS' | 'NOT_EQUALS';
-    /**
-     * The value to use for the condition.
-     * Example: completed
-     */
-    value: string;
-}
+export type CaseClassificationDocumentRequirementStatus = 'OK' | 'INSUFFICIENT_DOCUMENTS';
 
 export interface DateField {
     /**
--- src/resources/MachineLearning/FilterConditions.ts
+++ src/resources/MachineLearning/FilterConditions.ts
@@ -1,8 +1,3 @@
-export enum Operator {
-    Equals = 'EQUALS',
-    NotEquals = 'NOT_EQUALS',
-}
-
 export interface FilterConditions {
     /**
      * The name of the field.
@@ -13,7 +8,7 @@ export interface FilterConditions {
      * The operator to use to evaluate the condition.
      * Must be EQUALS or NOT_EQUALS
      */
-    operator: Operator;
+    operator: 'EQUALS' | 'NOT_EQUALS';
     /**
      * The value to use for the condition.
      * Example: completed
