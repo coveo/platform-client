dd6e203d3b0779073c8caed0d32f203bf94c7847
Gaël Dostie <gdostie@coveo.com>
2022-10-17 17:36:47 -0400

    feat: expose Resource and API classes

--- src/Entry.ts
+++ src/Entry.ts
@@ -2,10 +2,10 @@ import PlatformClient from './PlatformClient';
 
 export * from './PlatformClient';
 export * from './ConfigurationInterfaces';
-export * from './APICore';
 export * from './handlers';
 export * from './resources';
 export * from './utils';
 export * from './Endpoints';
 
+export {default as API} from './APICore';
 export default PlatformClient;
