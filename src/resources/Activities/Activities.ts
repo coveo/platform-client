e64e9839c4a959a0b2cc243e235d8e1c5c0ef402
simonmilord <smilord@coveo.com>
2022-10-20 17:32:39 -0400

    feat(activities): added call to list activities from all orgs

--- src/resources/Activities/Activities.ts
+++ src/resources/Activities/Activities.ts
@@ -11,6 +11,7 @@ import {
 
 export default class Activity extends Resource {
     static getBaseUrl = () => `/rest/organizations/${API.orgPlaceholder}/activities`;
+    static getBaseUrlAllOrgs = () => `/rest/activities`;
 
     get(activityId: string) {
         return this.api.get<ActivityModel>(`${Activity.getBaseUrl()}/${activityId}`);
@@ -29,6 +30,13 @@ export default class Activity extends Resource {
     listFacets(params?: ListActivitiesFacetsParams, activityFacet?: ActivityListingFilters) {
         return this.api.post<ActivityFacetModel>(
             this.buildPath(`${Activity.getBaseUrl()}/facets/public`, params),
+            activityFacet
+        );
+    }
+
+    listAll(params?: ListActivitiesParams, activityFacet?: ActivityListingFilters) {
+        return this.api.post<PageModel<ActivityModel>>(
+            this.buildPath(`${Activity.getBaseUrlAllOrgs()}`, params),
             activityFacet
         );
     }
