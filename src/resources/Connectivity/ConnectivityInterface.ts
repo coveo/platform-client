af7844c56d5a56e9390a2de80c69efcd47aeb0c9
Francois Rivard <frivard@coveo.com>
2022-10-24 14:11:42 -0400

    feat: crawling module log requests

--- src/resources/Connectivity/ConnectivityInterface.ts
+++ src/resources/Connectivity/ConnectivityInterface.ts
@@ -17,3 +17,37 @@ export enum LogRequestResourceType {
     SOURCE = 'SOURCE',
     SECURITY_PROVIDER = 'SECURITY_PROVIDER',
 }
+
+export enum CrawlingModuleLogRequestLogType {
+    MAESTRO = 'MAESTRO',
+    WORKER = 'WORKER',
+}
+
+export enum CrawlingModuleLogRequestState {
+    CREATED = 'CREATED',
+    ERROR = 'ERROR',
+    RUNNING = 'RUNNING',
+    SUCCESSFUL = 'SUCCESSFUL',
+    TIMEOUT = 'TIMEOUT',
+}
+
+export interface CrawlingModuleLogRequestModel {
+    createdDate: number;
+    id: string;
+    instanceId: string;
+    logType: CrawlingModuleLogRequestLogType;
+    operationId: string;
+    state: CrawlingModuleLogRequestState;
+}
+
+export interface CreateCrawlingModuleLogRequestModel {
+    instanceId: string;
+    logType: CrawlingModuleLogRequestLogType;
+    operationId: string;
+}
+
+export interface CrawlingModuleLogRequestDownloadModel {
+    info: string;
+    state: CrawlingModuleLogRequestState;
+    url: string;
+}
