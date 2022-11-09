ad6391f9f2dd14c07d3c84d6f704b4a16a8fde92
thudon-coveo <94566483+thudon-coveo@users.noreply.github.com>
2022-11-09 14:54:53 -0500

    fix(product listing): export product listing configuration functions and interfaces

--- src/resources/Catalogs/index.ts
+++ src/resources/Catalogs/index.ts
@@ -3,3 +3,5 @@ export * from './CatalogConfiguration';
 export * from './CatalogInterfaces';
 export * from './ProductListing';
 export * from './ProductListingInterfaces';
+export * from './ProductListingConfiguration';
+export * from './ProductListingConfigurationInterfaces';
