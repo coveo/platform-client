dde335751c8500469b26dee9c911f44257dd01b7
Charles-Auguste Marois <cmarois@coveo.com>
2022-11-11 17:26:52 -0500

    feat(products): add refreshCache parameter to products post call (#561)
    * feat(products): add refreshCache parameter to products post call

--- src/resources/Products/Product.ts
+++ src/resources/Products/Product.ts
@@ -6,7 +6,10 @@ import {ProductsRequestModel, ProductsResultModel} from './ProductInterfaces';
 export default class Products extends Resource {
     static baseUrl = `/rest/organizations/${API.orgPlaceholder}/commerce/v1/products`;
 
-    get(productsRequest: New<ProductsRequestModel>) {
-        return this.api.post<ProductsResultModel>(`${Products.baseUrl}/listing`, productsRequest);
+    get(productsRequest: New<ProductsRequestModel>, refreshCache = false) {
+        return this.api.post<ProductsResultModel>(
+            this.buildPath(`${Products.baseUrl}/listing`, {refreshCache: refreshCache}),
+            productsRequest
+        );
     }
 }
