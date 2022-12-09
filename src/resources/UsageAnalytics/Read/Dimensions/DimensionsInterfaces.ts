1b5c23adc36ffaa345c16136a4e3a49f538baf3e
Felix <fblais@coveo.com>
2022-12-09 10:08:17 -0500

    fix(dimensions): +add JSDoc on interfaces

--- src/resources/UsageAnalytics/Read/Dimensions/DimensionsInterfaces.ts
+++ src/resources/UsageAnalytics/Read/Dimensions/DimensionsInterfaces.ts
@@ -51,11 +51,23 @@ export interface DimensionValuesModel {
 }
 
 export interface CustomDimensionSuggestionsModel {
+    /**
+     * Suggestions of custom dimensions that could be created
+     */
     suggestions: CustomDimensionSuggestionModel[];
 }
 export interface CustomDimensionSuggestionModel {
+    /**
+     * The event type of the custom dimension
+     */
     eventName?: string;
+    /**
+     * The api name of the custom dimension
+     */
     apiName?: string;
+    /**
+     * The existing values for the custom dimension
+     */
     values?: string[];
 }
 
