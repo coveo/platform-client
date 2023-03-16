d9ea78bf4a654272305daf28d1cfa5014ca0c324
Louis Bompart <lbompart@coveo.com>
2023-03-16 12:08:46 -0400

    feat: add error class (#645)
    * feat!: add error class
    
    * docs: jsdoc

--- src/handlers/ResponseHandlers.ts
+++ src/handlers/ResponseHandlers.ts
@@ -18,7 +18,11 @@ const successBlob: ResponseHandler = {
 const error: ResponseHandler = {
     canProcess: () => true,
     process: async <T>(response: Response): Promise<T> => {
-        throw await response.json();
+        const responseJson = await response.json();
+        const platformError = new CoveoPlatformClientError();
+        Object.assign(platformError, responseJson);
+        platformError.xRequestId = response.headers.get('X-Request-ID') ?? 'unknown';
+        throw platformError;
     },
 };
 
@@ -27,3 +31,11 @@ export const ResponseHandlers = {noContent, success, successBlob, error};
 
 export default <T>(response: Response, handlers = defaultResponseHandlers) =>
     handlers.filter((handler) => handler.canProcess(response))[0].process<T>(response);
+
+export class CoveoPlatformClientError extends Error {
+    /**
+     * The HTTP X-Request-ID request header is an optional and unofficial HTTP header, used to trace individual HTTP requests from the client to the server and back again.
+     * It allows the client and server to correlate each HTTP request.
+     */
+    xRequestId: string;
+}
