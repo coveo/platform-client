f5ddbcaea3b4445beda057bddd164973c86619fb
JTangCoveo <113382897+JTangCoveo@users.noreply.github.com>
2023-11-13 10:26:34 -0500

    feat(ml)!: Enforce trackingIds parameter in the creation of PQS and IAPR model (#762)
    feat(ml): enforce trackingIds parameter in the creation of PQS and IAPR model
    
    BREAKING CHANGE: For creation of PQS / IAPR models, trackingIds will be a mandatory parameter to be
    included

--- src/resources/MachineLearning/IAPRConfiguration/IAPRConfigurationInterfaces.ts
+++ src/resources/MachineLearning/IAPRConfiguration/IAPRConfigurationInterfaces.ts
@@ -8,4 +8,10 @@ export interface IAPRConfigurationModel {
      * The model display name in the Coveo Administration console.
      */
     modelDisplayName: string;
+    /**
+     * The trackingIds that usage analytics events must contain for the model to use those events in its learning process. The model will use an event if it contains at least one of the specified IDs
+     *
+     * @Example: [ "sport" ]
+     */
+    trackingIds: string[];
 }
