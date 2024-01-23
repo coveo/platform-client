7e272545f04cbb174482273b60d9243c8d9dfc90
Felix <fblais@coveo.com>
2024-01-23 10:35:18 -0500

    feat(invites): +JsDoc on invites

--- src/resources/Invites/Invites.ts
+++ src/resources/Invites/Invites.ts
@@ -3,7 +3,9 @@ import {AuthInviteModel} from './Invites.model.js';
 
 export default class Invites extends Resource {
     static baseUrl = '/rest/invites';
-
+    /**
+     * Lists groups current user is invited too.
+     */
     list() {
         return this.api.get<AuthInviteModel[]>(Invites.baseUrl);
     }
