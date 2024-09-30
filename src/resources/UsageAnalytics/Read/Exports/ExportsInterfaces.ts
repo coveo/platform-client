a099a95c1ad9553860681b365f65418474d03b79
Marie Payne <116597610+mpayne-coveo@users.noreply.github.com>
2024-09-30 16:09:30 -0400

    feat: add exports with a request body [UA-8951] (#859)
    * feat: add exports with a request body [UA-8951]
    
    * fix: add missing annotations, async/await in exports tests [UA-8951]
    
    * fix: elaborated on annotations, added descriptions from Swagger, cleaned up interfaces [UA-8951]
    
    * fix: address comments [UA-8951]

--- src/resources/UsageAnalytics/Read/Exports/ExportsInterfaces.ts
+++ src/resources/UsageAnalytics/Read/Exports/ExportsInterfaces.ts
@@ -1,4 +1,4 @@
-import {CSVFileFormat, DayOfWeek, ExportScheduleFrequency, ExportStatus, ExportTablesType} from '../../../Enums.js';
+import {CSVFileFormat, DayOfWeek, ExportScheduleFrequency, ExportStatus, ExportTables} from '../../../Enums.js';
 import {
     EventDimensionsExcludeFilterParamParts,
     EventDimensionsFilterParamParts,
@@ -45,7 +45,7 @@ export interface EstimateExportParams extends TimeRangeParamParts, EventDimensio
      * The tables that will be included.
      * If not provided, all tables will be included.
      */
-    tables?: ExportTablesType[];
+    tables?: ExportTables[];
     /**
      * The filter that will be applied to the click and search events dimensions.
      * Multiple filter parameters are joined with the AND operator.
@@ -87,10 +87,89 @@ export interface GenerateExportParams extends EstimateExportParams {
     useDisplayNames?: boolean;
 }
 
+interface BaseGenerateExportWithBodyParams {
+    /**
+     * The beginning of the date range, in ISO8601 format (`YYYY-MM-DDThh:mm:ss.sssZ`).
+     */
+    from: string;
+    /**
+     * The end of the date range, in ISO8601 format (`YYYY-MM-DDThh:mm:ss.sssZ`).
+     */
+    to: string;
+    /**
+     * The file name to use for the export. Can only contain US-ASCII characters. By default, the export ID is used.
+     */
+    filename?: string;
+    /**
+     * An optional description for the export.
+     */
+    description?: string;
+    /**
+     * The dimensions to export. By default, all dimensions are exported.
+     */
+    dimensions?: string[];
+    /**
+     * The format of the generated CSV files. Defaults to `Excel`.
+     */
+    exportFormat?: CSVFileFormat;
+    /**
+     * Whether to use the display names in the export's header. Defaults to `false`, meaning that the API names are used by default.
+     */
+    useDisplayNames?: boolean;
+}
+
+export interface GenerateExportWithBodyParams extends BaseGenerateExportWithBodyParams {
+    /**
+     * The filters to apply to dimensions shared by all types of events.
+     * Multiple filter parameters are joined with the AND operator.
+     */
+    commonFilters?: string[];
+    /**
+     * The filters to apply to the dimensions of click and search events only.
+     * Multiple filter parameters are joined with the AND operator.
+     */
+    searchesFilters?: string[];
+    /**
+     * The filters to apply to the dimensions of custom events only.
+     * Multiple filter parameters are joined with the AND operator.
+     */
+    customEventsFilters?: string[];
+    /**
+     * The tables to export. By default, all tables are exported.
+     */
+    tables?: ExportTables[];
+}
+
+export interface GenerateVisitExportWithBodyParams extends BaseGenerateExportWithBodyParams {
+    /**
+     * The filter that will be applied to the events dimensions.
+     * Multiple filter parameters are joined with the AND operator.
+     */
+    inclusionFilters?: string[];
+    /**
+     * Each specified filter is inverted in order to hide events.
+     * Multiple filter parameters are joined with the AND operator.
+     */
+    hideEventFilters?: string[];
+    /**
+     * The filter that will be applied to dimensions to exclude events from the results.
+     * Multiple filter parameters are joined with the AND operator.
+     * The exclusion filter has precedence over the inclusion filter.
+     */
+    exclusionFilters?: string[];
+    /**
+     * Whether to order the rows by datetime in the export.
+     * If there are a large number of rows to export, this parameter will be ignored and the data will be unsorted.
+     * Defaults to `true`.
+     */
+    ordered?: boolean;
+}
+
 export interface GenerateVisitExportParams extends GenerateExportParams {
     /**
      * Whether to order the rows by datetime in the export.
-     * If the number of rows exported is too large, this parameter will be ignored and data will be unsorted.
+     * If there are a large number of rows to export, this parameter will be ignored and the data will be unsorted.
+     * Defaults to `true`.
      */
     ordered?: boolean;
 }
@@ -118,7 +197,7 @@ export interface CreateExportScheduleModel extends ExportScheduleModelCommonProp
     searchesFilters?: string[];
     customEventsFilters?: string[];
     description?: string;
-    tables?: ExportTablesType[];
+    tables?: ExportTables[];
     exportFormat?: CSVFileFormat;
 }
 
