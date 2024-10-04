72f75d2f883cf6848e0fdf7278167d4c5475763f
Alexandre Perron <aperron@coveo.com>
2024-10-04 16:45:02 -0400

    fix(UA-9417): make daysToExpiry param optional + add response model (#870)
    * fix(UA-9417): make daysToExpiry param optional + add response model
    
    * Update src/resources/UsageAnalytics/Read/Snowflake/SnowflakeInterfaces.ts
    
    Co-authored-by: Ruben Philippen <rphilippen@coveo.com>
    
    * fix(UA-9417): add tsdocs + fix signature of daysToExpiry
    
    ---------
    
    Co-authored-by: Ruben Philippen <rphilippen@coveo.com>

--- src/resources/UsageAnalytics/Read/Snowflake/Snowflake.ts
+++ src/resources/UsageAnalytics/Read/Snowflake/Snowflake.ts
@@ -1,6 +1,7 @@
 import ReadServiceResource from '../ReadServiceResource.js';
 import {
     GetCreditUsageParams,
+    ReactivateUserModel,
     ReactivateUserParams,
     SnowflakeCreditUsageModel,
     SnowflakeNetworkPolicyModel,
@@ -63,7 +64,7 @@ export default class Snowflake extends ReadServiceResource {
      * @param params The number of days until the user's expiration date.
      */
     reactivateUser(snowflakeUser: string, params: ReactivateUserParams) {
-        return this.api.put<void>(
+        return this.api.put<ReactivateUserModel>(
             this.buildPathWithOrg(`${Snowflake.baseUrl}/users/${snowflakeUser}/expiration`),
             params,
         );
