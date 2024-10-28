0089819e2b11a15513939104f786161ee29a4a8e
Felix <fblais@coveo.com>
2024-10-28 10:16:08 -0400

    feat(api-keys): +JsDoc

--- src/resources/ApiKeys/ApiKeysInterfaces.ts
+++ src/resources/ApiKeys/ApiKeysInterfaces.ts
@@ -114,8 +114,21 @@ export interface CreateApiKeyOptions {
 }
 
 export interface DuplicateApiKeyOptions {
+    /**
+     * The display name for the API key.
+     *
+     * **Example:** PushApiKey
+     */
     displayName?: string;
+    /**
+     * A brief description of the API key.
+     *
+     * **Example:** API key used for managing sources.
+     */
     description?: string;
+    /**
+     * The lifetime duration of the API key (iso8601 format).
+     */
     lifetimeDuration: string;
 }
 
