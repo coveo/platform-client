a5b653a6d603863e45983e5028ae66c1ab6db562
FelixBlaisThon <52010042+FelixBlaisThon@users.noreply.github.com>
2024-11-26 16:01:57 -0500

    feat(api-keys): +listing apikey eligibility call endpoint

--- src/resources/ApiKeyTemplate/ApiKeyTemplate.ts
+++ src/resources/ApiKeyTemplate/ApiKeyTemplate.ts
@@ -1,10 +1,16 @@
 import Resource from '../Resource.js';
-import {ApiKeyTemplateModel} from './ApiKeyTemplateInterface.js';
+import {ApiKeyTemplateEligibilityResponseModel, ApiKeyTemplateModel} from './ApiKeyTemplateInterface.js';
 
 export default class ApiKeyTemplate extends Resource {
     static baseUrl = '/rest/templates/apikeys';
 
     get(apiKeyTemplateId: string) {
         return this.api.get<ApiKeyTemplateModel>(`${ApiKeyTemplate.baseUrl}/${apiKeyTemplateId}`);
+    }
+
+    listAPIKeysEligibility() {
+        return this.api.get<ApiKeyTemplateEligibilityResponseModel[]>(
+            `${ApiKeyTemplate.baseUrl}/privileges/eligibility`,
+        );
     }
 }
