b6e73c9ec8df392b77524271620861989bce49d4
FelixBlaisThon <52010042+FelixBlaisThon@users.noreply.github.com>
2024-12-02 15:52:01 -0500

    fix(api-key): fix baseurl (#890)

--- src/resources/ApiKeyTemplate/ApiKeyTemplate.ts
+++ src/resources/ApiKeyTemplate/ApiKeyTemplate.ts
@@ -1,8 +1,9 @@
+import API from '../../APICore.js';
 import Resource from '../Resource.js';
 import {ApiKeyTemplateEligibilityResponseModel, ApiKeyTemplateModel} from './ApiKeyTemplateInterface.js';
 
 export default class ApiKeyTemplate extends Resource {
-    static baseUrl = '/rest/templates/apikeys';
+    static baseUrl = `/rest/organizations/${API.orgPlaceholder}/templates/apikeys`;
 
     get(apiKeyTemplateId: string) {
         return this.api.get<ApiKeyTemplateModel>(`${ApiKeyTemplate.baseUrl}/${apiKeyTemplateId}`);
