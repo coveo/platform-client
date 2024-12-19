90652889d404897c059e5755905ac38ed3ebbf7b
Paul Gerold <geroldpaul@gmail.com>
2024-12-19 11:05:53 -0500

    feat(MLX): deprecated UserActionHistory class

--- src/resources/MachineLearning/UserActionHistoryConfiguration/UserActionHistoryConfiguration.ts
+++ src/resources/MachineLearning/UserActionHistoryConfiguration/UserActionHistoryConfiguration.ts
@@ -1,17 +1,29 @@
 import API from '../../../APICore.js';
 import Resource from '../../Resource.js';
 
+/**
+ * @deprecated Will be removed from the API in January 2025
+ */
 export default class UserActionHistoryConfiguration extends Resource {
     static baseUrl = `/rest/organizations/${API.orgPlaceholder}/machinelearning/configuration/useractionhistory`;
 
+    /**
+     * @deprecated
+     */
     create() {
         return this.api.post<void>(UserActionHistoryConfiguration.baseUrl);
     }
 
+    /**
+     * @deprecated
+     */
     delete() {
         return this.api.delete<void>(UserActionHistoryConfiguration.baseUrl);
     }
 
+    /**
+     * @deprecated
+     */
     get() {
         return this.api.get<boolean>(UserActionHistoryConfiguration.baseUrl);
     }
