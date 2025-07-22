18e8c87d62fb61acc0a3376b940224a94d616c3b
Ruben Philippen <rphilippen@coveo.com>
2025-07-22 11:22:03 +0200

    feat(analytics-admin): add api additions and align naming
    UA-10349

--- src/resources/AnalyticsAdmin/tests/Properties.spec.ts
+++ src/resources/AnalyticsAdmin/tests/Properties.spec.ts
@@ -1,5 +1,6 @@
 import API from '../../../APICore.js';
 import Properties from '../Properties/Properties.js';
+import {ListPropertiesRequest} from '../Properties/PropertiesInterfaces.js';
 
 jest.mock('../../../APICore.js');
 
@@ -13,26 +14,69 @@ describe('Properties', () => {
         properties = new Properties(api, serverlessApi);
     });
 
-    describe('listProperties', () => {
+    describe('list', () => {
         it('should make a GET call to the list path', async () => {
             await properties.list();
 
             expect(api.get).toHaveBeenCalledTimes(1);
-            expect(api.get).toHaveBeenCalledWith(`${Properties.baseUrl}/properties/list`);
+            expect(api.get).toHaveBeenCalledWith(`${Properties.baseUrl}/properties`);
         });
 
         it('should make a GET call to the list path with pagination', async () => {
             await properties.list({page: 1, perPage: 2});
 
             expect(api.get).toHaveBeenCalledTimes(1);
-            expect(api.get).toHaveBeenCalledWith(`${Properties.baseUrl}/properties/list?page=1&perPage=2`);
+            expect(api.get).toHaveBeenCalledWith(`${Properties.baseUrl}/properties?page=1&perPage=2`);
         });
 
-        it('should make a GET call to the list path with filter', async () => {
-            await properties.list({filter: 'test'});
+        it('should make a GET call to the list path with filter and includeDefault', async () => {
+            await properties.list({filter: 'test', includeDefault: false});
 
             expect(api.get).toHaveBeenCalledTimes(1);
-            expect(api.get).toHaveBeenCalledWith(`${Properties.baseUrl}/properties/list?filter=test`);
+            expect(api.get).toHaveBeenCalledWith(`${Properties.baseUrl}/properties?filter=test&includeDefault=false`);
+        });
+
+        it('should make a GET call to the list path with projectId and includeProjectIds', async () => {
+            await properties.list({includeProjectIds: false, projectId: 'my-project'});
+
+            expect(api.get).toHaveBeenCalledTimes(1);
+            expect(api.get).toHaveBeenCalledWith(
+                `${Properties.baseUrl}/properties?includeProjectIds=false&projectId=my-project`,
+            );
+        });
+
+        it('should make a GET call to the list path with trackingIds', async () => {
+            await properties.list({trackingIds: ['id-1', 'id-2']});
+
+            expect(api.get).toHaveBeenCalledTimes(1);
+            expect(api.get).toHaveBeenCalledWith(`${Properties.baseUrl}/properties?trackingIds=id-1&trackingIds=id-2`);
+        });
+    });
+
+    describe('query', () => {
+        it('should make a POST call to the list path without body', async () => {
+            await properties.query();
+
+            expect(api.post).toHaveBeenCalledTimes(1);
+            expect(api.post).toHaveBeenCalledWith<[string, ListPropertiesRequest?]>(
+                `${Properties.baseUrl}/properties`,
+                undefined,
+            );
+        });
+
+        it('should make a POST call to the list path with body', async () => {
+            const params: ListPropertiesRequest = {
+                includeDefault: false,
+                includeProjectIds: true,
+                trackingIds: ['id-1', 'id-2'],
+            };
+            await properties.query(params);
+
+            expect(api.post).toHaveBeenCalledTimes(1);
+            expect(api.post).toHaveBeenCalledWith<[string, ListPropertiesRequest?]>(
+                `${Properties.baseUrl}/properties`,
+                params,
+            );
         });
     });
 
