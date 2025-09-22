2960cf27633f425245aa83c82779deda458d9126
Paul Gerold <geroldpaul@gmail.com>
2025-09-22 10:45:37 -0400

    fix: update body_markdown

--- src/resources/Enums.ts
+++ src/resources/Enums.ts
@@ -87,9 +87,9 @@ export enum OperationType {
 export enum DataStreamType {
     BODY_TEXT = 'BODY_TEXT',
     BODY_HTML = 'BODY_HTML',
+    BODY_MARKDOWN = 'BODY_MARKDOWN',
     THUMBNAIL = 'THUMBNAIL',
     DOCUMENT_DATA = 'DOCUMENT_DATA',
-    MARKDOWN = 'MARKDOWN',
 }
 
 export enum ExtensionLanguageType {
